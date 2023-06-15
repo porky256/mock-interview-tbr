396ccf6ca12b606e96d856ad53ca62001799752e
porky256 <61063240+porky256@users.noreply.github.com>
2023-06-15 22:09:58 +0200

    Restructure folders (#5)
    * restructure folders
    
    * fix lint errors
    
    ---------
    
    Co-authored-by: Nikita Pashko <pashkonikita.coop@gmail.com>

---
+++ internal/skill/model/model_skill.go
@@ -0,0 +1,7 @@
+package model
+
+type Skill struct {
+	ID          int64  `json:"id,omitempty"`
+	Name        string `json:"name,omitempty"`
+	Description string `json:"description,omitempty"`
+}
---
+++ internal/user/model/model_user.go
@@ -0,0 +1,22 @@
+package model
+
+import "github.com/porky256/mock-interview-tbr/internal/skill/model"
+
+type User struct {
+	ID          int64        `json:"id,omitempty"`
+	Username    string       `json:"username,omitempty"`
+	FirstName   string       `json:"firstName,omitempty"`
+	LastName    string       `json:"lastName,omitempty"`
+	Email       string       `json:"email,omitempty"`
+	Password    string       `json:"password,omitempty"`
+	Phone       string       `json:"phone,omitempty"`
+	UserStatus  int32        `json:"userStatus,omitempty"`
+	Description string       `json:"description,omitempty"`
+	UsersSkills []UsersSkill `json:"usersSkills,omitempty"`
+}
+
+type UsersSkill struct {
+	ID    int64        `json:"id,omitempty"`
+	Skill *model.Skill `json:"skill,omitempty"`
+	Score int32        `json:"score,omitempty"`
+}
